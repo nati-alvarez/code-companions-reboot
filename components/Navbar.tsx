19d1037388b890ee8dfd560bf0641cfd473af734
Natividad Alvarez <tivialvarez@gmail.com>
2021-01-23 07:50:30 -0500

    Made basic skeleton for Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+import styles from "@styles/Navbar.module.scss";
+import Link from "next/link";
+
+export default function Navbar(){
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles["nav-section"]}>
+                <Link href="/">
+                    <a className={styles.logo}>Code_Companions</a>
+                </Link>
+            </div>
+            <div className={styles["nav-section"]}>
+                <Link href="/social-hub">
+                    <a>Social Hub</a>
+                </Link>
+                <Link href="/blog">
+                    <a>Blog</a>
+                </Link>
+                <Link href="/login-signup">
+                    <a>Login/Signup</a>
+                </Link>
+            </div>
+        </nav>
+    )
+}
