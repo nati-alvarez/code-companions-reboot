4374ccfb336c6fed8241683e7bb5e14be5b636ce
Natividad Alvarez <tivialvarez@gmail.com>
2021-01-23 07:50:53 -0500

    Added linkes to google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="shortcut icon" href='/favicons/favicon.ico'/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;500;600&display=swap" rel="stylesheet"/>
       </Head>
       <Component {...pageProps} />
     </>
