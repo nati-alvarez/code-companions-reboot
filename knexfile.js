75bfc62db1d1ed8ca02b54126e44f416eaac9f0f
Natividad Alvarez <tivialvarez@gmail.com>
2021-01-28 13:56:04 -0500

    Setup knex connection to local db

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
