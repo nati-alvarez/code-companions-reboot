1b219da364efff087b551ead9cb8256de3e01969
Natividad Alvarez <tivialvarez@gmail.com>
2021-02-01 13:52:09 -0500

    Added github login button to login-signup page; login-signup page receives githubAccess Token

--- pages/login-signup.tsx
+++ pages/login-signup.tsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import axios from "axios";
 
 //styles
 import styles from "@styles/LoginSignup.module.scss";
@@ -8,8 +9,49 @@ import Form from "@components/Form";
 
 //components
 import Navbar from "@components/Navbar";
+import GithubAuthButton from "@components/login-signup/GithubAuthButton";
+import { NextPageContext } from "next";
 
-export default function LoginSignup(){
+
+export async function getServerSideProps(context: NextPageContext) {
+    const githubAccessCode: string[] | string = context.query.code
+    const props: {githubAccessToken? : string, formStartingWith? : string | string[]} = {}
+    if(context.query.state) props.formStartingWith = context.query.state;
+
+    if(githubAccessCode){
+        try {
+            const res =  await axios.post(
+                "https://github.com/login/oauth/access_token",
+                {
+                    client_id: process.env.GITHUB_CLIENT_ID,
+                    client_secret: process.env.GITHUB_CLIENT_SECRET,
+                    code: githubAccessCode,
+                },
+                {headers: {"Accept": "application/json"}}
+            )
+            const githubAccessToken = res.data.access_token ? res.data.access_token: null;
+            props.githubAccessToken = githubAccessToken;
+        }catch(err){
+            console.log(err)
+        }
+    }
+    return {
+        props
+    }
+}
+
+
+export default function LoginSignup({githubAccessToken, formStartingWith}){
+    const [form, setForm] = useState<string>(formStartingWith? formStartingWith: "signup")
+    //putting this prop in state so it can be cleared
+    //we want to clear it in the event the user swaps forms.
+    const [githubAccessTokenState, setGithubAccessTokenState] = useState<string>(githubAccessToken);
+
+    const loginAction = async (data) => {
+        const res = await axios.post("/api/login", data);
+        console.log(res);
+    }
+
     const LoginForm = <Form 
         heading="Login"
         fields={[
@@ -17,7 +59,7 @@ export default function LoginSignup(){
             {label: "password", name: "password", inputType: "password", validationType: "password"}
         ]} 
         buttonText="Login" 
-        action={(fieldState)=>console.log(fieldState)}
+        action={loginAction}
     />
     const SignupForm = <Form
         heading="Signup"
@@ -31,24 +73,40 @@ export default function LoginSignup(){
             console.log(fieldState)
         }}
     />
-    const [form, setForm] = useState<string>("signup")
     
+    function swapForm(form: string): void{
+        setForm(form);
+        setGithubAccessTokenState(null);
+    }
+
     return (
         <div>
             <Navbar/>
             <main className={styles.main}>
                 <p className={styles.prompt}>Welcome! Please login or sign up to get started</p>
                 <div className={styles["form-container"]}>
+                    {githubAccessTokenState &&
+                        <div className={styles['form-success']}>
+                            Success! Your Github account connected. Continue to fill out the form below.
+                        </div>
+                    }
                     <div className={styles["form-tabs"]}>
-                        <button className={form === "login"? styles.active: ""} onClick={()=> setForm("login")}data-testid="login-tab">
+                        <button className={form === "login"? styles.active: ""} onClick={()=> swapForm("login")}data-testid="login-tab">
                             Login
                         </button>
-                        <button className={form === "signup"? styles.active: ""} onClick={()=> setForm("signup")} data-testid="signup-tab">
+                        <button className={form === "signup"? styles.active: ""} onClick={()=> swapForm("signup")} data-testid="signup-tab">
                             Signup
                         </button>
                     </div>
-                    {form == "login" && LoginForm}
-                    {form == "signup" && SignupForm}
+                    {form === "login" && LoginForm}
+                    {form === "signup" && SignupForm}
+                    <div className={styles["github-auth"]}>
+                    {form === "login"?
+                            <GithubAuthButton redirectState="login" buttonText="Login with Github"/>
+                        :
+                            <GithubAuthButton redirectState="signup" buttonText="Connect Github Account"/>
+                    }
+                    </div>
                 </div>
             </main>
         </div>
