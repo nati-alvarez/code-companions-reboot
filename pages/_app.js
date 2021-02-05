a377fd2cca6ec8ac202b10917ab50e20015a4738
Natividad Alvarez <tivialvarez@gmail.com>
2021-02-05 15:20:47 -0500

    Added error and success messages to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,24 @@
 import '../styles/globals.css'
 import Head from "next/head";
+import {Provider} from "jotai";
+
+//components
+import GlobalError from "@components/GlobalError";
+import GlobalSuccess from "@components/GlobalSuccess";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider>
       <Head>
         <link rel="shortcut icon" href='/favicons/favicon.ico'/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;500;600&display=swap" rel="stylesheet"/>
       </Head>
+      <GlobalSuccess/>
+      <GlobalError/>
       <Component {...pageProps} />
-    </>
+    </Provider>
   )
 }
 
-export default MyApp
+export default MyApp;
