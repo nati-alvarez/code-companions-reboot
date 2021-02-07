ca2a349d25419aa00f5b21f4def9726a0e75164f
Natividad Alvarez <tivialvarez@gmail.com>
2021-02-07 16:43:43 -0500

    Completed api/login route

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,16 +1,39 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import UsersModel from "@models/Users";
+import {serialize} from "cookie";
+import {generateAuthToken, generateRefreshToken} from "@helpers/jwt";
+
 
 interface LoginReqBody {
     email : string
     password: string
 }
 
-export default function(req: NextApiRequest, res: NextApiResponse){
+export default async function(req: NextApiRequest, res: NextApiResponse){
     switch(req.method){
         case "POST":
             const {email, password} : LoginReqBody = req.body;
             if(!email || !password) return res.status(400).json({message: "Username and password required"});
-            res.status(200).json({message: "logging you in"});
+            try {
+                const userPayload = await UsersModel.getUserLogin(email, password);
+                const refreshToken = generateRefreshToken(userPayload);
+                const authToken = generateAuthToken(userPayload);
+                const today = new Date();
+                const cookieExpiryDate = today.setDate(today.getDate() + 2);
+                res.setHeader(
+                    "Set-Cookie",
+                    serialize("rt", refreshToken, {
+                        httpOnly: true,
+                        expires: new Date(cookieExpiryDate),
+                        maxAge:  60 * 60 * 24 * 2,
+                        path: "/",
+                        secure: process.env.NODE_ENV === "production"
+                    })
+                );
+                res.status(200).json({message: "Login successful", authToken});
+            }catch(err){
+                res.status(400).json({message: err.message})
+            }
             break;
     }
 }
