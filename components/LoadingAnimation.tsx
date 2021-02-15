6c4abb4a03bdfbacc36832998ffda774e69cd904
Natividad Alvarez <tivialvarez@gmail.com>
2021-02-14 19:52:24 -0500

    Added testid to LoadingAnimation component

--- components/LoadingAnimation.tsx
+++ components/LoadingAnimation.tsx
@@ -2,7 +2,7 @@ import styles from "@styles/LoadingAnimation.module.scss";
 
 export default function LoadingAnimation(){
     return (
-        <div className={styles.container}>
+        <div data-testid="loading-animation" className={styles.container}>
             <div className={styles.circle}></div>
         </div>
     )
