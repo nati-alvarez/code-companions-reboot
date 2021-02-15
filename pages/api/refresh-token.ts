41099032e1bbb1442cd043f2064530a9274b422e
Natividad Alvarez <tivialvarez@gmail.com>
2021-02-14 20:40:42 -0500

    Using cookie npm package to parse cookies

--- pages/api/refresh-token.ts
+++ pages/api/refresh-token.ts
@@ -1,9 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import {verifyRefreshToken} from "@helpers/jwt";
+import cookie from "cookie";
 
 export default function(req: NextApiRequest, res: NextApiResponse){
     if(!req.headers.cookie) return res.status(400).json({message: "Invalid token"});
-    const refreshToken = req.headers.cookie.substr(req.headers.cookie.indexOf("=") + 1);
+    const refreshToken = cookie.parse(req.headers.cookie).rt;
     let token = verifyRefreshToken(refreshToken);
     if(typeof token !== "string") return res.status(400).json({message: token.message});
     res.status(200).json({token});
