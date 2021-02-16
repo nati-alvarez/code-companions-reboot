e36a2726ddeb53f87e49d92439d996b915136c75
Natividad Alvarez <tivialvarez@gmail.com>
2021-02-15 21:16:52 -0500

    Updated home page to show page listings and projects

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,11 +1,46 @@
-import Navbar from "@components/Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 //styles
 import styles from "@styles/Home.module.scss";
 
+interface Project {
+    projectId: number
+    title: string
+    description: string
+    ownerId: number
+    ownerUsername: string
+    ownerProfilePicture: string | null
+}
+
+//components
+import Navbar from "@components/Navbar";
+import MyProjects from "@components/home/MyProjects";
+import ProjectListings from "@components/home/ProjectListings";
+import LoadingAnimation from "@components/LoadingAnimation";
+import { JWTAuthTokenAtom } from "@atoms/auth";
+import { useAtom } from "jotai";
+
 export default function Home(){
+    const [JWTToken, setJWTToken] = useAtom(JWTAuthTokenAtom);
     const [tab, setTab] = useState("projects");
+    const [myProjectsLoading, setMyProjectsLoading] = useState(false);
+    const [myProjects, setMyProjects] = useState<Array<Project>>([]);
+    const [projectListings, setProjectListings] = useState<Array<object>>([]);
+
+    useEffect(()=>{
+        setMyProjectsLoading(true);
+        axios.get("/api/projects", {headers: {
+            "Authorization": JWTToken
+        }}).then(res=>{
+            setMyProjects(res.data.projects); 
+        }).catch(err=>{
+            console.log(err.response.data.message);
+        }).finally(()=>{
+            setMyProjectsLoading(false);
+        })
+    }, []);
+
     return (
         <div>
             <Navbar/>
@@ -19,6 +54,17 @@ export default function Home(){
                 className={tab == "listings"? `${styles['dash-tab']} ${styles['active']}` :styles["dash-tab"]}>
                     Project Listings
                 </div>
+            </div>
+            <div className={styles["dash-container"]}>
+                {tab === "projects"?
+                    myProjectsLoading?
+                        <LoadingAnimation/>
+                    :
+                        <MyProjects projects={myProjects}/>
+                    
+                :
+                    <ProjectListings listings={projectListings}/>
+                }
             </div>
             </main>
         </div>
