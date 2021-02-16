c55fe103e225b943d0dbee3105f9862b9b9f7a6a
Natividad Alvarez <tivialvarez@gmail.com>
2021-02-15 21:18:19 -0500

    Wrote method to get a users projects

--- models/Projects.ts
+++ models/Projects.ts
@@ -15,6 +15,13 @@ export default abstract class Projects {
         .returning("id, title, description");
     }
 
+    static getUserProjects(userId: number){
+        return db("Projects as p")
+        .leftJoin("Users as u", "u.id", 'p.ownerId')
+        .where({ownerId: userId})
+        .select("p.id as projectId", "p.title", "p.description", "u.id as ownerId", "u.username as ownerUsername", "u.profilePicture as ownerProfilePicture");
+    }
+
     static async projectExists(title : string, ownerId : number){
         db("Projects").where({title, ownerId}).first();
     }
