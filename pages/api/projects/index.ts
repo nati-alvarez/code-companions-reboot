ab91fcbb68af0cf73654baed845ccd1292137079
Natividad Alvarez <tivialvarez@gmail.com>
2021-02-21 17:32:57 -0500

    Added endpoint to create a new project

--- pages/api/projects/index.ts
+++ pages/api/projects/index.ts
@@ -7,10 +7,19 @@ export default async function(req: NextApiRequest, res: NextApiResponse){
     switch(req.method){
         case "GET":
             try{
-                authenticateUser(req, res);   
-                console.log(req['user']);
+                authenticateUser(req, res);
                 const projects = await ProjectsModel.getUserProjects(req['user'].id);
                 res.status(200).json({projects});
+            }catch(err){
+                res.status(401).json({message: err.message});
+            }
+            break;
+        case "POST":
+            try {
+                authenticateUser(req, res);
+                const {title, description, github_repo} = req.body;
+                const project = await ProjectsModel.createProject({title, description, github_repo, ownerId: req["user"].id});
+                res.status(201).json({message: "Project created", project});
             }catch(err){
                 res.status(401).json({message: err.message});
             }
