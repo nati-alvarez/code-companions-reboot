e0eb6d1f0995f600f9f4f0622eb8f7cdf8222df0
Natividad Alvarez <tivialvarez@gmail.com>
2021-03-01 05:03:04 -0500

    passing in query and option parameters to getListigns method

--- pages/api/listings/index.ts
+++ pages/api/listings/index.ts
@@ -11,10 +11,12 @@ export default async function (req: NextApiRequest, res: NextApiResponse){
     switch(req.method){
         case "GET":
             try {
+                const {query, option} = req.query;
                 authenticateUser(req, res);
-                const listings = await ListingsModel.getListings();
+                const listings = await ListingsModel.getListings({query: query as string, option: option as string});
                 res.status(200).json({listings});
             }catch(err){
+                console.log(err)
                 res.status(401).json({message: err.message});
             }
             break;
