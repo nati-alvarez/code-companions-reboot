29636aab67aca9d7a5c5e30516db65593c1f4b01
Natividad Alvarez <tivialvarez@gmail.com>
2021-03-01 03:05:58 -0500

    Filtering posts based on keyword query

--- models/Listings.ts
+++ models/Listings.ts
@@ -1,12 +1,22 @@
 import db from "./db";
 
+interface ListingSearchFilters {
+    query: string,
+    option: string
+}
+
 export default abstract class ListingsModel {
-    static async getListings(){
+    static async getListings(filters: ListingSearchFilters){
         let listings = await db("Listings as l")
             .join("Projects as p", "p.id", "l.projectId")
             .join("Users as u", "u.id", "p.ownerId")
             .select(["l.id", "l.listingTitle", "l.description", "l.created_at", "u.id as ownerId", "u.username as ownerUsername", "u.profilePicture as ownerProfilePicture"])
-            .where({"l.public": true});
+            .where({"l.public": true})
+            .modify(function(queryBuilder) {
+                if (filters.query) {
+                    queryBuilder.whereRaw('LOWER("listingTitle") LIKE ?', `%${filters.query.toLowerCase()}%`);
+                }
+            }); 
         
         for (let listing of listings){
             try {
@@ -16,7 +26,7 @@ export default abstract class ListingsModel {
                     
                 listing.skills = skills;
             }catch(err){
-                //don't break app just don't show any required skills
+                //don't throw error, just don't show any required skills
                 console.log(err, err.message);
                 listing.skills = [];
             }
