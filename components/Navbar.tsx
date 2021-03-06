0842673c753008a9138c75152fd91533d4d3802e
Natividad Alvarez <tivialvarez@gmail.com>
2021-03-06 16:16:11 -0500

    Navbar showing my profile link when user is logged in

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,17 @@
-import styles from "@styles/Navbar.module.scss";
 import Link from "next/link";
+import jwt from "jsonwebtoken";
+
+//styles
+import styles from "@styles/Navbar.module.scss";
+
+//atoms
+import {useAtom} from "jotai";
+import {JWTAuthTokenAtom} from "@atoms/auth";
 
 export default function Navbar(){
+    const [jwtToken, setJwtToken] = useAtom(JWTAuthTokenAtom);
+    const user = jwt.decode(jwtToken);
+
     return (
         <nav className={styles.navbar}>
             <div className={styles["nav-section"]}>
@@ -19,9 +29,15 @@ export default function Navbar(){
                 <Link href="/blog">
                     <a>Blog</a>
                 </Link>
-                <Link href="/login-signup">
-                    <a>Login/Signup</a>
-                </Link>
+                {!user &&
+                    <Link href="/login-signup">
+                        <a>Login/Signup</a>
+                    </Link>
+                }{user &&
+                    <Link href="/profile">
+                        <a>My Profile</a>
+                    </Link>
+                }
             </div>
         </nav>
     )
