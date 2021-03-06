57b33545c3235f36a5266dafedf6a6ede138b513
Natividad Alvarez <tivialvarez@gmail.com>
2021-03-06 18:31:04 -0500

    Set api/refresh-token route to only work on POST requests

--- pages/api/refresh-token.ts
+++ pages/api/refresh-token.ts
@@ -3,9 +3,16 @@ import {verifyRefreshToken} from "@helpers/jwt";
 import cookie from "cookie";
 
 export default function(req: NextApiRequest, res: NextApiResponse){
-    if(!req.headers.cookie) return res.status(400).json({message: "Invalid token"});
-    const refreshToken = cookie.parse(req.headers.cookie).rt;
-    let token = verifyRefreshToken(refreshToken);
-    if(typeof token !== "string") return res.status(400).json({message: token.message});
-    res.status(200).json({token});
+    switch(req.method){
+        case "POST":
+            if(!req.headers.cookie) return res.status(400).json({message: "Invalid token"});
+            const refreshToken = cookie.parse(req.headers.cookie).rt;
+            let token = verifyRefreshToken(refreshToken);
+            if(typeof token !== "string") return res.status(400).json({message: token.message});
+            res.status(200).json({token});
+            break;
+        default:
+            res.status(404).send("Page not found");
+    }
+   
 }
