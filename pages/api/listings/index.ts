afc42527964242b3bee576d349d5699a66270678
Natividad Alvarez <tivialvarez@gmail.com>
2021-03-06 18:30:30 -0500

    using pagination query strings passed to GET /api/listings

--- pages/api/listings/index.ts
+++ pages/api/listings/index.ts
@@ -11,10 +11,14 @@ export default async function (req: NextApiRequest, res: NextApiResponse){
     switch(req.method){
         case "GET":
             try {
-                const {query, option} = req.query;
                 authenticateUser(req, res);
-                const listings = await ListingsModel.getListings({query: query as string, option: option as string});
-                res.status(200).json({listings});
+                let {query, option, page, listingsPerPage} = req.query as {query: string, option: string, page: string|number, listingsPerPage: string|number};
+                //using undefined here because the default param values in .getListings for page and listingsPerPage won't be used if null is passed into params
+                page = Number.isNaN(parseInt(page as string))? undefined: parseInt(page as string);
+                listingsPerPage = Number.isNaN(parseInt(listingsPerPage as string))? undefined: parseInt(listingsPerPage as string);
+                
+                const searchResults = await ListingsModel.getListings({query, option, page, listingsPerPage});
+                res.status(200).json({listings: searchResults.listings, totalListings: searchResults.totalListings});
             }catch(err){
                 console.log(err)
                 res.status(401).json({message: err.message});
