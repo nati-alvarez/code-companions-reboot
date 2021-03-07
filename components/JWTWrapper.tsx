7c4dc9a0402f367f3416b2a45b699856d8140723
Natividad Alvarez <tivialvarez@gmail.com>
2021-03-07 14:48:56 -0500

    Fixed redirect logic on page path update

--- components/JWTWrapper.tsx
+++ components/JWTWrapper.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import {useRouter} from "next/router";
 import jwt from "jsonwebtoken";
 import axios from "axios";
@@ -14,8 +14,11 @@ export default function JWTWrapper({children} : {children: React.ReactChild}){
     const [JWTAuthToken, setJWTAuthToken] = useAtom(JWTAuthTokenAtom);
     const refreshTokenInterval = useRef<any>();
     const router = useRouter();
-    const restrictedRoutes = ["/home", "/profile"];
-
+    const restrictedRoutes = ["/home", "/profile", "/projects/[id]"];
+    //the hasPageLoaded state will be used to ensure initial check for JWT on component mount is finished
+    //before running the check on router path change. The check for router path change runs on
+    //page load for some reason and causes an unecessary "double refresh"
+    const [hasPageLoaded, setHasPageLoaded] = useState(false);
     async function getNewAuthToken(){
         return axios.post("/api/refresh-token", {}, {withCredentials: true});
     }
@@ -30,8 +33,18 @@ export default function JWTWrapper({children} : {children: React.ReactChild}){
             console.log(err.response.data.message);
             console.log("token, expired. redirecting...");
             if(restrictedRoutes.includes(router.pathname)) router.replace("/login-signup");
+        }).finally(()=>{
+            setHasPageLoaded(true)
         });
     }, []);
+
+    //checks if protected routes should be loaded or redirected 
+    useEffect(()=>{
+        if(hasPageLoaded){
+            if(router.pathname === "/login-signup" && JWTAuthToken) router.replace("/home");
+            if(restrictedRoutes.includes(router.pathname) && !JWTAuthToken) router.replace("/login-signup");
+        }
+    }, [router.pathname]);
 
     useEffect(()=>{
         if(JWTAuthToken){
