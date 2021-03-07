755ec4db1bc16c79beb58db2f76235df958b2c3b
Natividad Alvarez <tivialvarez@gmail.com>
2021-03-06 19:33:08 -0500

    Added option for number of listings per page to ProjectListings form

--- components/home/ProjectListings.tsx
+++ components/home/ProjectListings.tsx
@@ -13,7 +13,8 @@ export default function ProjectListings({setGlobalSuccessMessage, setGlobalError
     const [loading, setLoading] = useState(true);
     const [listingFilters, setListingFilters] = useState({
         query: null,
-        option: "newest"
+        option: "newest",
+        listingsPerPage: 10
     });
     useEffect(()=>{
         axios.get(
@@ -39,7 +40,7 @@ export default function ProjectListings({setGlobalSuccessMessage, setGlobalError
     const getListings = async () => {
         setLoading(true);
         //null will be passed in as the string "null" so checking here it before adding it to url
-        let queryString = `?option=${listingFilters.option}` 
+        let queryString = `?option=${listingFilters.option}&listingsPerPage=${listingFilters.listingsPerPage}` 
         queryString += (listingFilters.query)? `&query=${listingFilters.query}`: "";
         try {
             const res = await axios.get(
@@ -62,13 +63,33 @@ export default function ProjectListings({setGlobalSuccessMessage, setGlobalError
             <h3>Projects for you:</h3>
             <div className={styles["listings-filters"]}>
                 <fieldset>
-                    <label>Search by title: </label>
-                    <input onKeyUp={(e)=> {if(e.keyCode === 13) getListings()}} onChange={e => setListingFilters({...listingFilters, query: e.target.value})} />
+                    <label htmlFor="query">Search by title: </label>
+                    <input 
+                        id="query" 
+                        onKeyUp={(e)=> {if(e.keyCode === 13) getListings()}}
+                        onChange={e => setListingFilters({...listingFilters, query: e.target.value})}
+                        value={listingFilters.query}
+                    />
                 </fieldset>
-                <select onChange={e => setListingFilters({...listingFilters, option: e.target.value})}>
-                    <option value="newest">Newest</option>
-                    <option value="oldest">Oldest</option>
-                </select>
+                <fieldset>
+                    <label htmlFor="order-by">Order by: </label>
+                    <select id="order-by" onChange={e => setListingFilters({...listingFilters, option: e.target.value})}>
+                        <option value="newest">Newest</option>
+                        <option value="oldest">Oldest</option>
+                    </select>
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="per-page">Listings per page: </label>
+                    <input 
+                        id="per-page"
+                        type="number" 
+                        max="20" min="1"
+                        defaultValue="10"
+                        onChange={e => setListingFilters({...listingFilters, listingsPerPage: parseInt(e.target.value)})}
+                        value={listingFilters.listingsPerPage}
+                    />
+                </fieldset>
+
                 <button onClick={getListings} className={styles["filter-button"]}>Find</button>
             </div>
             {loading?
