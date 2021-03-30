87794666df3a1e48f37b3bb77f0c6f0446350b37
Natividad Alvarez <tivi@pop-os.localdomain>
2021-03-30 08:48:58 -0400

    performing auth middleware at start of all routes for projects/index.ts

--- pages/api/projects/index.ts
+++ pages/api/projects/index.ts
@@ -4,24 +4,27 @@ import ProjectsModel from "@models/Projects";
 import {authenticateUser} from "@helpers/jwt";
 
 export default async function(req: NextApiRequest, res: NextApiResponse){
+    try {
+        authenticateUser(req, res);
+    }catch(err){
+        res.status(401).json({message: err.message});
+    }
     switch(req.method){
         case "GET":
             try{
-                authenticateUser(req, res);
                 const projects = await ProjectsModel.getUserProjects(req['user'].id);
                 res.status(200).json({projects});
             }catch(err){
-                res.status(401).json({message: err.message});
+                res.status(500).json({message: err.message});
             }
             break;
         case "POST":
             try {
-                authenticateUser(req, res);
                 const {title, description, github_repo} = req.body;
                 const project = await ProjectsModel.createProject({title, description, github_repo, ownerId: req["user"].id});
                 res.status(201).json({message: "Project created", project});
             }catch(err){
-                res.status(401).json({message: err.message});
+                res.status(500).json({message: err.message});
             }
     }
 }
