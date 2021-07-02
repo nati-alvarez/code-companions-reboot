305424330c8b894b8f27ed1efd717e5e3a30a0bd
Natividad <tivialvarez@gmail.com>
2021-07-02 06:42:59 -0400

    extracted more of the functionality of refreshing the access token into getNewAuthToken

--- components/JWTWrapper.tsx
+++ components/JWTWrapper.tsx
@@ -24,13 +24,9 @@ export default function JWTWrapper({children} : {children: React.ReactChild}){
     //before running the check on router path change. The check for router path change runs on
     //page load for some reason and causes an unecessary "double refresh"
     const [hasPageLoaded, setHasPageLoaded] = useState(false);
+
     async function getNewAuthToken(){
-        return axios.post("/api/refresh-token", {}, {withCredentials: true});
-    }
-    
-    //gets token on page/window refresh
-    useEffect(()=>{
-        getNewAuthToken().then(res=>{
+        return axios.post("/api/refresh-token", {}, {withCredentials: true}).then(res=>{
             setJWTAuthToken(res.data.token);
             if(router.pathname === "/login-signup") router.replace("/home");
         }).catch(err=>{
@@ -38,7 +34,13 @@ export default function JWTWrapper({children} : {children: React.ReactChild}){
             console.log(err.response.data.message);
             console.log("token, expired. redirecting...");
             if(restrictedRoutes.includes(router.pathname)) router.replace("/login-signup");
-        }).finally(()=>{
+            setJWTAuthToken("");
+        });
+    }
+    
+    //gets token on page/window refresh
+    useEffect(()=>{
+        getNewAuthToken().finally(()=>{
             setHasPageLoaded(true)
         });
     }, []);
@@ -54,32 +56,12 @@ export default function JWTWrapper({children} : {children: React.ReactChild}){
     useEffect(()=>{
         if(JWTAuthToken){
             //redirects on initial login
-            console.log(router.pathname)
             if(router.pathname === "/login-signup") router.replace("/home");
             const {exp, iat} = jwt.decode(JWTAuthToken);
             //sets interval to refresh token once token exprires
             refreshTokenInterval.current = setInterval(()=>{
-                getNewAuthToken().then(res=>{
-                    console.log("auth token refreshed after expiry")
-                    setJWTAuthToken(res.data.token)
-                    if(router.pathname === "/login-signup") router.replace("/home");
-                }).catch(err=>{
-                    //cookie expired, redirects home
-                    if(restrictedRoutes.includes(router.pathname)) router.replace("/login-signup");
-                    setJWTAuthToken("")
-                });
+                getNewAuthToken();
             }, (exp - iat) * 1000);
-        }else {
-            // //on refresh, attempts to refresh token with cookie
-            // getNewAuthToken().then(res=>{
-            //     setJWTAuthToken(res.data.token);
-            //     if(router.pathname === "/login-signup") router.replace("/home");
-            // }).catch(err=>{
-            //         //cookie expired, redirects home
-            //         console.log(err.response.data.message);
-            //         console.log("token, expired. redirecting...");
-            //         router.replace("/login-signup");
-            //     });
         }
         return ()=> clearInterval(refreshTokenInterval.current);
     }, [JWTAuthToken]);
