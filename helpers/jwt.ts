5571e7084d01c7b5107cd80167bb9e28a4d63252
Natividad <tivialvarez@gmail.com>
2021-07-02 07:04:43 -0400

    Added jsdocs to jwt helper file

--- helpers/jwt.ts
+++ helpers/jwt.ts
@@ -1,33 +1,56 @@
 import jwt from "jsonwebtoken";
 import {NextApiRequest, NextApiResponse} from "next";
 
-export function generateRefreshToken(payload: object){
-    return jwt.sign(payload, process.env.JWT_SECRET, {
+/**
+ * Generates the refresh token
+ * @param {object} payload - the payload to store in the JWT
+ * @returns {string} the json webtoken
+ */
+export function generateRefreshToken(payload: object) : string{
+    return jwt.sign(payload, process.env.REFRESH_JWT_SECRET, {
         expiresIn: "2d"
     });
 }
 
-export function generateAuthToken(payload: object){
-    return jwt.sign(payload, process.env.JWT_SECRET, {
+/**
+ * Generates the auth token
+ * @param {object} payload - the payload to store in the JWT
+ * @returns {string} the json webtoken
+ */
+export function generateAuthToken(payload: object) : string{
+    return jwt.sign(payload, process.env.AUTH_JWT_SECRET, {
         expiresIn: "20m"
     });
 }
 
-export function verifyRefreshToken(refreshToken: string){
+/**
+ * Verifies the refresh token. Returns token when successful and Error instance when unsuccessful
+ * @param {string} refreshToken - the refresh token to verify
+ * @returns {string} the JWT when verification is successful
+ * @returns {Error} an Error instance when verification is unsuccessful
+ */
+export function verifyRefreshToken(refreshToken: string) : Error | string {
     try{
-        const data = jwt.verify(refreshToken, process.env.JWT_SECRET);
+        const data = jwt.verify(refreshToken, process.env.REFRESH_JWT_SECRET);
         return generateAuthToken({id: data.id, username: data.username});
     }catch(err){
         return new Error("Invalid token");
     }
 }
 
-export function authenticateUser(req: NextApiRequest, res: NextApiResponse){
+/**
+ * Authenticates the auth token. Sets user info in payload to request object when successful
+ * and returns Error instance when unsuccessful
+ * @param {NextApiRequest} req - Next API request object
+ * @param {NextApiResponse} res - Next API response object
+ * @returns {Error} an Error instance when verification is unsuccessful
+ */
+export function authenticateUser(req: NextApiRequest, res: NextApiResponse) : undefined | Error {
     const token = req.headers.authorization;
     try{
-        const user = jwt.verify(token, process.env.JWT_SECRET);
+        const user = jwt.verify(token, process.env.AUTH_JWT_SECRET);
         req['user'] = user; //using bracket syntax, otherwise typescript complains
     }catch(err){
-        throw new Error("Invalid token");
+        return new Error("Invalid token");
     }
 }
