b8edd3db57be801e57ea024995acda4483936165
Natividad Alvarez <tivialvarez@gmail.com>
2021-08-03 09:11:30 -0400

    Changed PUT user\[id] endpoint to return updated user object

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -32,13 +32,14 @@ export default async function(req: NextApiRequest, res: NextApiResponse){
                 break;
             case "PUT":
                 try {
-                    const {id} = req.query;
+                    const id : string = req.query.id as string;
                     // When calling .update() on knex models, if a field is unique and you try chainging it to 
                     // a value that already exists in the db you get a unique constraint error EVEN IF the field of the record being edited holds
                     // that unique value. This line will prevent that error
                     if(req["user"].username === req.body.username) delete req.body.username;
                     await UsersModel.updateUserInfo(id, req.body);
-                    res.status(200).json({message: "We hit the user info edit endpoint"});
+                    const user = await UsersModel.getMyProfile(parseInt(id));
+                    res.status(200).json({message: "Your profile was updated successfuly", user});
                 }catch(err){
                     console.log(err);
                     res.status(500).json({message: err.message});
